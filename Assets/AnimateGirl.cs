626fa7cdd2654d8439439943d8e8f74d249fcd5a
Jdevvv <llanta.jeremy@gmail.com>
2020-09-30 16:06:06 +0200

    first commit

---
+++ Assets/AnimateGirl.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator), typeof(SpriteRenderer))]
+public class AnimateGirl : MonoBehaviour
+{
+
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    public float maxSpeed = 2;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+
+        Vector3 move = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            move += Vector3.right * maxSpeed;
+            spriteRenderer.flipX = false;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            move += Vector3.left * maxSpeed;
+            spriteRenderer.flipX = true;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            move += Vector3.up * maxSpeed;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            move += Vector3.down * maxSpeed;
+        }
+
+        animator.SetFloat("Speed", Mathf.Abs(move.magnitude));
+        this.transform.position = this.transform.position + (Vector3)move;
+    }
+}
