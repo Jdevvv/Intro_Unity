667b0cda0ef53f0b5f1f19d32a374b74615dc0c1
jerem <llanta.jeremy@gmail.com>
2020-10-28 16:47:59 +0100

    start Score text mesh

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+public class Score : MonoBehaviour
+{
+    public GameObject orange_girl;
+    public GameObject blue_girl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        orange_girl = GameObject.Find("girl-2_0");
+        blue_girl = GameObject.Find("girl-2_0_2");
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "ballPrefab")
+        {
+            if (gameObject.name == "Left Goal")
+            {
+                Debug.Log("Left goal");
+
+            }
+            if (gameObject.name == "Right Goal")
+            {
+                Debug.Log("Right goal");
+            }
+        }
+        Debug.Log(col.gameObject.name + " : " + gameObject.name);
+    }
+
+}
